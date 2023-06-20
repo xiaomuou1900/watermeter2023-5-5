563ddf0543ff024adf8224a723e8d5e99ef492f1
xiaomuou1900 <304910918@qq.com>
2023-06-20 16:25:33 +0800

    2023/6/30

--- sources/js/home.js
+++ sources/js/home.js
@@ -1,118 +1,395 @@
-// var testSumData = {}
-//   , startTime = 0
-// $.ajax({
-//   type: 'get'
-//   , url: '../../api/transfer_devices.php'
-//   , data: {
-//     page: 1
-//   }
-//   , success: function (res) {
-//     testSumData = {
-//       testingNum: res.testingNum,
-//       testPaseNum: res.testPaseNum,
-//       testFailNum: res.testFailNum,
-//       totalTestNum: res.totalTestNum,
-//       startTime: res.startTime
-//     };
-//     var htmlTestSum = template('tpl-testSum', testSumData);
-//     $('#testSum').html(htmlTestSum);                                   //....渲染测试总数....//
-//   }
-// })
 
-$.ajax({
-  type: 'get'
-  , url: '../../api/transfer_devices.php'
-  , data: {
-    page: 1
-  }
-  , success: function (res) {
-    testSumData = {
-      testingNum: res.testingNum,
-      testPaseNum: res.testPaseNum,
-      testFailNum: res.testFailNum,
-      totalTestNum: res.totalTestNum,
-      startTime: res.startTime
-    };
-    var htmlTestSum = template('tpl-testSum', testSumData);
-    $('#testSum').html(htmlTestSum);                                   //....渲染测试总数....//
-  }
-})
-
-var table_data = []
-layui.use(['laypage', 'layer', 'table', 'form'], function () {
+layui.use(['laypage', 'layer', 'table', 'form','element'], function () {
   var table = layui.table
     , laypage = layui.laypage
     , layer = layui.layer
-    , form = layui.form;
+    , form = layui.form
+    , element = layui.element;
 
-  table.render({       //渲染正在测试界面表格
-    elem: '#testDevice'
-    , url: '../../api/transfer_devices.php'
-    , page: {
-      limit: 25
-      , limits: [10, 15, 20, 25, 30, 35, 40, 45, 50]
-      , layout: ['prev', 'page', 'next', 'skip', 'count', 'limit']
-      , jump: function (obj, first) {                                        //分页被操作时触发该函数
-        //obj包含了当前分页的所有参数，比如：
-        console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
-        console.log(obj.limit); //得到每页显示的条数
-        //首次不执行
-        if (!first) {
-          //do something
-          table.reloadData('testDevice', {                                   //只重载表格数据
-            where: {
-              page: obj.curr
-              , limit: obj.limit
-            }
-          });
+  $.ajax({
+    type: 'get'
+    , url: '../../api/home.json'
+    , data: {
+      get:'homePageFourCount'
+    }
+    , success: function (res) {
+      if(res.code == 0) {
+        $('#meterCount_id #value1').html(res.meterTotalCount)
+        $('#meterCount_id #value2').html(res.meterUsedCount)
+        $('#meterCount_id #value3').html(res.zeroFlowUserCount)
+        $('#meterCount_id #value4').html(res.arrearsUserCount)
+      } else {
+        $('#meterCount_id #value1').html('<div style="font-size:14px; color:yellow">加载失败</div>')
+        $('#meterCount_id #value2').html('<div style="font-size:14px; color:yellow">加载失败</div>')
+        $('#meterCount_id #value3').html('<div style="font-size:14px; color:yellow">加载失败</div>')
+        $('#meterCount_id #value4').html('<div style="font-size:14px; color:yellow">加载失败</div>')
+      }
+    }
+  })
+
+  $.ajax({
+    type: 'get'
+    , url: '../../api/home.json'
+    , data: {
+      get:'deviceStatusAlarmCount'
+    }
+    , success: function (res) {
+      if(res.code == 0) {
+        $('#deviceStatusAlarm .label-danger').html(res.deviceStatusAlarmCount)
+      } else {
+        layer.alert('获取设备状态报警数量失败')
+      }
+    }
+  })
+  $('#deviceStatusAlarm button').on('click', function() {        //...设备状态报警 按钮...//
+    $('#deviceStatusAlarm #tableDiv').toggleClass('hide')
+    $.ajax({
+      type: 'get'
+      , url: '../../api/home.json'
+      , data: {
+        get:'deviceStatusAlarmCount'
+      }
+      , success: function (res) {
+        if(res.code == 0) {
+          $('#deviceStatusAlarm .label-danger').html(res.deviceStatusAlarmCount)
+        } else {
+          layer.alert('获取设备状态报警数量失败')
         }
       }
+    })
+    if($('#deviceStatusAlarm button').html() == '+') {
+      $('#deviceStatusAlarm button').html("<span style='font-size:35px'>-</span>")
+      table.render({             //...渲染设备状态报警表格...//
+        elem: '#deviceStatusAlarm_table'
+        , url: '../../api/home.json'
+        , page: {
+          limit: 25
+          , limits: [10, 15, 20, 25, 30, 35, 40, 45, 50]
+          , layout: ['prev', 'page', 'next', 'count', 'limit', 'skip']
+          , jump: function (obj, first) {                                        //分页被操作时触发该函数
+            //obj包含了当前分页的所有参数，比如：
+            console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+            console.log(obj.limit); //得到每页显示的条数
+            //首次不执行
+            if (!first) {
+              //do something
+              table.reloadData('testDevice', {                                   //只重载表格数据
+                where: {
+                  page: obj.curr
+                  , limit: obj.limit
+                  ,get: "data_deviceStatusAlarm"
+                }
+              });
+            }
+          }
+        }
+        , where:{
+          get: "data_deviceStatusAlarm"
+        }
+        , parseData: function (res) { //res 即为原始返回的数据
+          return {
+            "code": res.code, //解析接口状态
+            "msg": res.message, //解析提示文本
+            "count": res.count, //解析数据长度
+            "data": res.data_deviceStatusAlarm //解析数据列表
+          };
+        }
+        , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+        , cols: [[
+           { field: 'meterID', title: '设备编号', minWidth: 120 } //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
+          , { field: 'abnormalStatus', title: '异常状态', minWidth: 160}
+        ]]
+        , even: true
+        , done: function (res, curr, count) {                                 //表格渲染完后触发该函数
+          //如果是异步请求数据方式，res即为你接口返回的信息。
+          //如果是直接赋值的方式，res即为：{data: [], count: 99} data为当前页数据、count为数据总长度
+          // console.log(res);
+    
+          //得到当前页码
+          // console.log(curr); 
+        }
+      });
+    } else {
+      $('#deviceStatusAlarm button').html("+")
+    }
+  })
+
+  $.ajax({
+    type: 'get'
+    , url: '../../api/home.json'
+    , data: {
+      get:'userStatusAlarmCount'
+    }
+    , success: function (res) {
+      if(res.code == 0) {
+        $('#userStatusAlarm .label-danger').html(res.userStatusAlarmCount)
+      } else {
+        layer.alert('获取设备状态报警数量失败')
+      }
+    }
+  })
+  $('#userStatusAlarm button').on('click', function() {        //...用户状态报警 按钮...//
+    $('#userStatusAlarm #tableDiv').toggleClass('hide')
+    $.ajax({
+      type: 'get'
+      , url: '../../api/home.json'
+      , data: {
+        get:'userStatusAlarmCount'
+      }
+      , success: function (res) {
+        if(res.code == 0) {
+          $('#userStatusAlarm .label-danger').html(res.userStatusAlarmCount)
+          $('#afterPayUser_span').html(res.aferPayUserAlarmCount)
+          $('#beforePayUser_span').html(res.beforPayUserAlarmCount)
+        } else {
+          layer.alert('获取用户状态报警数量失败')
+        }
+      }
+    })
+    if($('#userStatusAlarm button').html() == '+') {
+      $('#userStatusAlarm button').html("<span style='font-size:35px'>-</span>")
+      table.render({             //...渲染后付费用户表格...//
+        elem: '#afterPayUserStatusAlarm_table'
+        , url: '../../api/home.json'
+        , page: {
+          limit: 25
+          , limits: [10, 15, 20, 25, 30, 35, 40, 45, 50]
+          , layout: ['prev', 'page', 'next', 'count', 'limit', 'skip']
+          , jump: function (obj, first) {                                        //分页被操作时触发该函数
+            //obj包含了当前分页的所有参数，比如：
+            console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+            console.log(obj.limit); //得到每页显示的条数
+            //首次不执行
+            if (!first) {
+              //do something
+              table.reloadData('testDevice', {                                   //只重载表格数据
+                where: {
+                  page: obj.curr
+                  , limit: obj.limit
+                  ,get: "data_afterPayUserStatusAlarm"
+                }
+              });
+            }
+          }
+        }
+        , where:{
+          get: "data_afterPayUserStatusAlarm"
+        }
+        , parseData: function (res) { //res 即为原始返回的数据
+          return {
+            "code": res.code, //解析接口状态
+            "msg": res.message, //解析提示文本
+            "count": res.count, //解析数据长度
+            "data": res.data_afterPayUserStatusAlarm //解析数据列表
+          };
+        }
+        , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+        , cols: [[
+           { field: 'userID', title: '用户编号', minWidth: 120 } //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
+          , { field: 'userName', title: '用户名', minWidth: 160}
+          , { field: 'arrearage', title: '欠费金额（元）', minWidth: 160}
+        ]]
+        , even: true
+        , done: function (res, curr, count) {                                 //表格渲染完后触发该函数
+          //如果是异步请求数据方式，res即为你接口返回的信息。
+          //如果是直接赋值的方式，res即为：{data: [], count: 99} data为当前页数据、count为数据总长度
+          // console.log(res);
+    
+          //得到当前页码
+          // console.log(curr); 
+        }
+      });
+      table.render({             //...渲染预付费用户表格...//
+        elem: '#beforePayUserStatusAlarm_table'
+        , url: '../../api/home.json'
+        , page: {
+          limit: 25
+          , limits: [10, 15, 20, 25, 30, 35, 40, 45, 50]
+          , layout: ['prev', 'page', 'next', 'count', 'limit', 'skip']
+          , jump: function (obj, first) {                                        //分页被操作时触发该函数
+            //obj包含了当前分页的所有参数，比如：
+            console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+            console.log(obj.limit); //得到每页显示的条数
+            //首次不执行
+            if (!first) {
+              //do something
+              table.reloadData('testDevice', {                                   //只重载表格数据
+                where: {
+                  page: obj.curr
+                  , limit: obj.limit
+                  ,get: "data_beforePayUserStatusAlarm"
+                }
+              });
+            }
+          }
+        }
+        , where:{
+          get: "data_beforePayUserStatusAlarm"
+        }
+        , parseData: function (res) { //res 即为原始返回的数据
+          return {
+            "code": res.code, //解析接口状态
+            "msg": res.message, //解析提示文本
+            "count": res.count, //解析数据长度
+            "data": res.data_beforePayUserStatusAlarm //解析数据列表
+          };
+        }
+        , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+        , cols: [[
+           { field: 'userID', title: '用户编号', minWidth: 120 } //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
+          , { field: 'userName', title: '用户名', minWidth: 160}
+          , { field: 'arrearage', title: '欠费金额（元）', minWidth: 160}
+        ]]
+        , even: true
+        , done: function (res, curr, count) {                                 //表格渲染完后触发该函数
+          //如果是异步请求数据方式，res即为你接口返回的信息。
+          //如果是直接赋值的方式，res即为：{data: [], count: 99} data为当前页数据、count为数据总长度
+          // console.log(res);
+    
+          //得到当前页码
+          // console.log(curr); 
+        }
+      });
+      element.on('tab(test-handle)', function(data){           //...单击tab选项...//
+        // console.log(this); // 当前 tab 标题所在的原始 DOM 元素
+        // console.log(data.index); // 得到当前 tab 项的所在下标
+        // console.log(data.elem); // 得到当前的 tab 容器
+        $.ajax({
+          type: 'get'
+          , url: '../../api/home.json'
+          , data: {
+            get:'userStatusAlarmCount'
+          }
+          , success: function (res) {
+            if(res.code == 0) {
+              $('#userStatusAlarm .label-danger').html(res.userStatusAlarmCount)
+              $('#afterPayUser_span').html(res.aferPayUserAlarmCount)
+              $('#beforePayUser_span').html(res.beforPayUserAlarmCount)
+            } else {
+              layer.alert('获取用户状态报警数量失败')
+            }
+          }
+        })
+        if(data.index == 0) {
+          table.reloadData('afterPayUserStatusAlarm_table',{
+          where: {
+            get:'data_afterPayUserStatusAlarm'
+          }
+          })
+        } else if(data.index == 1) {
+          table.reloadData('beforePayUserStatusAlarm_table',{
+            where: {
+              get:'data_beforePayUserStatusAlarm'
+            }
+          })
+        }
+      });
+    } else {
+      $('#userStatusAlarm button').html("+")
+    }
+  })
+
+  $('#beforePayUserSearch #button_show').on('click', function() {        //...预付费用户余额筛选 显示隐藏按钮...//
+    $('#beforePayUserSearch #tableDiv').toggleClass('hide')
+    if($('#beforePayUserSearch #button_show').html() == '+') {
+      $('#beforePayUserSearch #button_show').html("<span style='font-size:35px'>-</span>")
+      $('#balance_input').val("")
+      table.render({             //...预付费用户余额筛选表格...//
+        elem: '#beforePayUserSearch_table'
+        , url: '../../api/home.json'
+        , page: {
+          limit: 25
+          , limits: [10, 15, 20, 25, 30, 35, 40, 45, 50]
+          , layout: ['prev', 'page', 'next', 'count', 'limit', 'skip']
+          , jump: function (obj, first) {                                        //分页被操作时触发该函数
+            //obj包含了当前分页的所有参数，比如：
+            console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+            console.log(obj.limit); //得到每页显示的条数
+            //首次不执行
+            if (!first) {
+              //do something
+              table.reloadData('testDevice', {                                   //只重载表格数据
+                where: {
+                  page: obj.curr
+                  , limit: obj.limit
+                  ,get: "data_beforePayUserSearch"
+                }
+              });
+            }
+          }
+        }
+        , where:{
+          get: "data_beforePayUserSearch"
+        }
+        , parseData: function (res) { //res 即为原始返回的数据
+          return {
+            "code": res.code, //解析接口状态
+            "msg": res.message, //解析提示文本
+            "count": res.count, //解析数据长度
+            "data": res.data_beforePayUserSearch //解析数据列表
+          };
+        }
+        , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+        , cols: [[
+          {field:'id', title:'序号', type:'numbers', minWidth:50}
+          , { field: 'userID', title: '用户编号', minWidth: 120 } //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
+          , { field: 'userName', title: '用户姓名', minWidth: 120}
+          , { field: 'meterID', title: '设备编号', minWidth: 160}
+          , { field: 'balance', title: '余额', minWidth: 120}
+          , { field: 'userAddress', title: '用户地址', minWidth: 160}
+        ]]
+        , even: true
+        , done: function (res, curr, count) {                                 //表格渲染完后触发该函数
+          //如果是异步请求数据方式，res即为你接口返回的信息。
+          //如果是直接赋值的方式，res即为：{data: [], count: 99} data为当前页数据、count为数据总长度
+          // console.log(res);
+    
+          //得到当前页码
+          // console.log(curr); 
+        }
+      });
+    } else {
+      $('#beforePayUserSearch #button_show').html("+")
+    }
+  })
+
+  $('#button_search').on('click', function() {    //...预付费用户余额筛选 筛选按钮...//
+    if($('#balance_input').val() == "") {
+      table.reloadData('beforePayUserSearch_table',{ 
+        where: {
+           get:'data_beforePayUserSearch'
+         }
+      })  
+    } else {
+      table.reloadData('beforePayUserSearch_table',{ 
+       where: {
+          get:'data_beforePayUserSearch'
+          ,balance_NotMoreThan: $('#balance_input').val()
+        }
+      })
+    }
+  })
+
+
+  table.render({             //...渲染抄表状态表格
+    elem: '#readMeterStatus_table'
+    , url: '../../api/home.json'
+    ,where:{
+      get:"data_readMeterStatus"
     }
     , parseData: function (res) { //res 即为原始返回的数据
       return {
-        "code": res.status, //解析接口状态
+        "code": res.code, //解析接口状态
         "msg": res.message, //解析提示文本
         "count": res.count, //解析数据长度
-        "data": res.data //解析数据列表
+        "data": res.data_readMeterStatus //解析数据列表
       };
     }
     , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
     , cols: [[
-      { type: 'checkbox', minWidth: 50 }
-      , { field: 'id', title: '序号', type: 'numbers', minWidth: 50 }
-      , { field: 'meterID', title: '设备编号', minWidth: 120, sort: true } //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
-      , { field: 'IMEI', title: 'IMEI', minWidth: 160, sort: true }
-      , { field: 'ICCID', title: 'ICCID', minWidth: 200, sort: true }
-      , { field: 'VDD', title: '电池电压', minWidth: 105, sort: true ,templet:function(d) {
-        var myHtml = ''
-        if(d.VDD < 3.3){
-          myHtml = '<span>电池电压<label style="background-color: rgb(206, 49, 49); border-radius: 10px; color: #fff; line-height: 20px; width: 60px; text-align: center;">'+ d.VDD +'V</label></span>'
-        }
-        else {
-          myHtml = '<span>电池电压<label style="background-color: rgb(31, 138, 31); border-radius: 10px; color: #fff; line-height: 20px; width: 60px; text-align: center;">'+ d.VDD +'V</label></span>'
-        }
-        return myHtml
-    }}
-      , { field: 'CSQ', title: '信号强度', minWidth: 105, sort: true ,templet:function(d) {
-        var myHtml = ''
-        if(d.CSQ < 10) {
-          myHtml = myHtml + '<span>信号强度<label style="background-color: rgb(206, 49, 49); border-radius: 10px; color: #fff; line-height: 20px; ; width: 20px;text-align: center;">'+ d.CSQ +'</label></span>'
-        }
-        else if(d.CSQ < 15) {
-          myHtml = myHtml + '<span>信号强度<label style="background-color: orange; border-radius: 10px; color: #fff; line-height: 20px; ; width: 20px;text-align: center;">'+ d.CSQ +'</label></span>'
-        }
-        else {
-          myHtml = myHtml + '<span>信号强度<label style="background-color:  rgb(31, 138, 31); border-radius: 10px; color: #fff; line-height: 20px; ; width: 20px;text-align: center;">'+ d.CSQ +'</label></span>'
-        }
-        return myHtml
-    }}
-      , { field: 'totalFlow', title: '累计用量', minWidth: 105, sort: true } //单元格内容水平居中
-      , { field: 'testFlow', title: '当前测试用量', minWidth: 130, sort: true } //单元格内容水平居右
-      , { field: 'reportTime', title: '最近上报时间', minWidth: 160, sort: true }
-      , { field: 'V', title: '版本号', minWidth: 100, sort: true }
-      , { field: 'testResult', title: '测试结果', minWidth: 105, sort: true }
-      , { field: 'testTime', title: '测试时间', minWidth: 160, sort: true }
-      , { field: 'operate', title: '操作', width: 125, minWidth: 125, minWidth: 100, templet: '#tpl-specificsBt' }
+       { field: 'thisReadMeterTime', title: '本次抄表日期', minWidth: 120 } //width 支持：数字、百分比和不填写。你还可以通过 minWidth 参数局部定义当前单元格的最小宽度，layui 2.2.1 新增
+      , { field: 'readSuccessMeterNum', title: '本次抄表进度（抄回表数/已在线表总数）', minWidth: 160}
+      , { field: 'nextReadMeterTime', title: '下次抄表日期', minWidth: 200 }
     ]]
     , even: true
     , done: function (res, curr, count) {                                 //表格渲染完后触发该函数
@@ -122,10 +399,6 @@ layui.use(['laypage', 'layer', 'table', 'form'], function () {
 
       //得到当前页码
       // console.log(curr); 
-
-      //得到数据总量
-      // console.log(count);
-      table_data = res.data
     }
   });
 
---
+++ sources/lib/hiprint.io/Content/hiprint/print-data.js
@@ -0,0 +1,11 @@
+var printData = {
+    userAddress: '喷泉小区26-6-602',
+    userID: '6666',
+    receiptNo:'888888',
+    chargeDate:'2023-6-20',
+    table: [
+        { lastReadTime: '2023-5-1', lastRead: '1000', thisReadTime: '2023-6-1', thisRead: '1100', waterUsage: '100', unitPrice: '3.3', money:'330'}
+    ],
+    moneyCapital:'叁佰叁拾元',
+    drawer:'李雷'
+};
